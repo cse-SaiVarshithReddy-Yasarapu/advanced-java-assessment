dcc51fd8df471fbfd6ccbd8ef2fd6cfe2213a48a
cse-SaiVarshithReddy-Yasarapu <cse.sai.varshith.reddy.yasarapu@gmail.com>
2026-02-14 15:29:31 +0530

    Add files via upload

---
+++ assessment/src/main/java/assessment/Main.java
@@ -0,0 +1,36 @@
+package assessment;
+
+
+import jakarta.persistence.*;
+
+public class Main {
+	public static void main( String[] args ) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("taskPU");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		Employee emp = new Employee("Sai", "svarshith@gmail.com");
+        IDCard card = new IDCard("IDC101", "2026-02-14",emp);
+
+        emp.setIdCard(card);
+        em.persist(card);   
+
+        em.getTransaction().commit();
+
+        System.out.println("Employee Created Successfully");
+
+        Employee fetchedEmp = em.find(Employee.class, 1L);
+        System.out.println("Employee Details:");
+        System.out.println("ID: " + fetchedEmp.getId());
+        System.out.println("Name: " + fetchedEmp.getName());
+        System.out.println("Email: " + fetchedEmp.getEmail());
+
+        IDCard fetchedCard = fetchedEmp.getIdCard();
+        System.out.println("\nID Card Details:");
+        System.out.println("Card Number: " + fetchedCard.getCardNumber());
+        System.out.println("Issue Date: " + fetchedCard.getIssueDate());
+
+        em.close();
+        emf.close();
+	}
+}
